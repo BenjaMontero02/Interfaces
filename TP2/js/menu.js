75573aa7b2adfa4dbcf1cb4034468abf8a4f16fa
Fidel Brasesco <fidelbrasesco@gmail.com>
2023-10-04 21:25:38 -0300

    reg, login y loader

--- TP2/js/menu.js
+++ TP2/js/menu.js
@@ -1,29 +1,40 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    let menu = document.getElementById('menu');
-    let navMenu = document.getElementById("navMenu");
-    let btnCloseMenu = document.getElementById("btn-close-menu");
+  let menu = document.getElementById("menu");
+  let navMenu = document.getElementById("navMenu");
+  let btnCloseMenu = document.getElementById("btn-close-menu");
 
-    let btnProfile = document.getElementById("profile");
-    let navMenuProfile = document.getElementById("navMenuProfile");
+  let btnProfile = document.getElementById("profile");
+  let navMenuProfile = document.getElementById("navMenuProfile");
 
-    menu.addEventListener("click", () => {
-        if (navMenu.style.display === "block") {
-            navMenu.style.display = "none";
-        } else {
-            navMenu.style.display = "block";
-        }
-    });
+  menu.addEventListener("click", () => {
+    if (navMenu.style.display === "block") {
+      navMenu.style.display = "none";
+    } else {
+      navMenu.style.display = "block";
+    }
+  });
 
-    btnCloseMenu.addEventListener("click", () => {
-        navMenu.style.display = "none";
-    });
+  btnCloseMenu.addEventListener("click", () => {
+    navMenu.style.display = "none";
+  });
 
-    btnProfile.addEventListener("click", () => {
-        if(navMenuProfile.style.display === "none") {
-            navMenuProfile.style.display = "block";
-        }else{
-            navMenuProfile.style.display = "none";
-        }
-    });
-
+  btnProfile.addEventListener("click", () => {
+    if (navMenuProfile.style.display === "none") {
+      navMenuProfile.style.display = "block";
+    } else {
+      navMenuProfile.style.display = "none";
+    }
+  });
 });
+function temporizadorDeRetraso() {
+  identificadorTiempoDeEspera = setTimeout(funcionConRetraso, 5000);
+  console.log("asdasd");
+}
+
+function funcionConRetraso() {
+  document.getElementById("onload").remove();
+  var div = document.getElementById("bodyRemove");
+  div.classList.remove("hidden");
+}
+
+temporizadorDeRetraso();
