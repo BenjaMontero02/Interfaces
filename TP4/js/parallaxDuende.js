f1d5fd9670a1784ffb5f4debd636f40f3c0e369c
cacha <monterobenja.2018@gmail.com>
2023-11-24 13:46:26 -0300

    add parallax banner and point 9

--- TP4/js/parallaxDuende.js
+++ TP4/js/parallaxDuende.js
@@ -2,18 +2,15 @@ let duende = document.getElementById("duende");
 
 document.addEventListener("scroll", () => {
     let scrollPosition = window.scrollY;
-    console.log("scroll position" + scrollPosition)
     let computedStyle = window.getComputedStyle(duende);
 
     let top = computedStyle.getPropertyValue('top');
     
     let value = parseFloat(top);
-    console.log(value);
     if(value > -95){
         let status = value - scrollPosition * 0.0099;
 
         if(status > -95){
-            console.log(status);
             duende.style.top = status + "px";
         }else{
             duende.style.top = -95 + "px";
